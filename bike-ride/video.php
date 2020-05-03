4a36ef0b37b42ad8928f2490a6191f7ef953e4b9
mokrijyurij <mokrijyurij@outlook.com>
2020-05-03 18:52:33 +0300

    add video

---
+++ bike-ride/video.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <title>BikeRide | Велопробег</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="loans HTML Template">
+    <meta name="keywords" content="loans, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="shortcut icon"/>
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
+          rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="css/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="css/flaticon.css"/>
+    <link rel="stylesheet" href="css/slicknav.min.css"/>
+
+    <!-- Main Stylesheets -->
+    <link rel="stylesheet" href="css/style.css"/>
+
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Header Section -->
+<header class="header-section">
+    <h3 class="text-white"><a href="index.html" class="site-logo">
+            BIKE-RIDE
+        </a></h3>
+    <nav class="header-nav">
+        <ul class="main-menu">
+            <li><a href="index.html" class="active">Главная</a></li>
+            <li><a href="about-us.html">О нас</a></li>
+            <li><a href="#">Заезды</a>
+                <ul class="sub-menu">
+                    <li><a href="about-us.html">Маршруты</a></li>
+                    <li><a href="loans.html">Участники</a></li>
+                    <li><a href="elements.html">Победители</a></li>
+                </ul>
+            </li>
+            <li><a href="news.html">Результаты</a></li>
+            <li><a href="contact.html">Видео</a></li>
+        </ul>
+        <div class="header-right">
+            <a href="#" class="hr-btn"><i class="flaticon-029-telephone-1"></i>Звоните! </a>
+            <div class="hr-btn hr-btn-2">+78 332 65767 42</div>
+        </div>
+    </nav>
+</header>
+<!-- Header Section end -->
+
+<!-- Hero Section end -->
+<section class="hero-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="hs-text">
+                    <h2>Регистрация на велопробег</h2>
+                    <p>Для участия в велопробеге введите в форму ниже свои данные</p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <form class="hero-form">
+                    <input type="text" name="username" placeholder="Ваше имя">
+                    <input type="text" name="email" placeholder="Электронаня почта">
+                    <input type="text" name="phone" placeholder="Your Phone">
+                    <p>Заполняя и отправляя форму вы соглашаетесь на обработку персональных данных. Детали по участию и
+                        условиям мы отправим на вашу электронную почту. </p>
+                    <button class="site-btn">Регистрация</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="hero-slider owl-carousel">
+        <div class="hs-item set-bg" data-setbg="img/hero-slider/1.jpg"></div>
+        <div class="hs-item set-bg" data-setbg="img/hero-slider/2.jpg"></div>
+        <div class="hs-item set-bg" data-setbg="img/hero-slider/3.jpg"></div>
+    </div>
+</section>
+<!-- Hero Section end -->
+
+<!-- CTA Section end -->
+<section class="cta-section set-bg" data-setbg="img/cta-bg.jpg">
+    <div class="container">
+        <h2>Видео наших велопробегов!</h2>
+    </div>
+</section>
+<!-- CTA Section end -->
+
+
+<!-- Feature Section -->
+<section class="feature-section spad">
+    <div class="container">
+        <div class="feature-item">
+            <div class="row">
+                <div class="col-lg-6">
+                    <iframe width="550" height="400"
+                            src="https://www.youtube.com/embed/ma7JQ9CE8Cw">
+                    </iframe>
+                </div>
+                <div class="col-lg-6">
+                    <div class="feature-text">
+                        <h2>На старт! Традиционный весенний велопробег</h2>
+                        <p>Любители крутить педали, на старт! Пятого мая в нашем городе прошёл очередной, условно
+                            открывающий сезон езды на велосипеде, пробег. В чём особенность нынешнего веломарафона,
+                            увидел Владимир Городнюк.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="feature-item">
+            <div class="row">
+                <div class="col-lg-6">
+                    <iframe width="550" height="400"
+                            src="https://www.youtube.com/embed/ngfFKpYi-8g">
+                    </iframe>
+                </div>
+                <div class="col-lg-6">
+                    <div class="feature-text">
+                        <h2>Велопробег. Брест 2019</h2>
+                        <p>Днем 18 мая 2019 г. на парковке у «звезды» Брестской крепости собрались любители здорового
+                            образа жизни. Сложно подсчитать общее количество участников велофестиваля (регистрация была
+                            необязательной), но по примерным нашим подсчетам где-то в районе тысячи велосипедистов. Это
+                            уже III-ий велосипедный фестиваль «Без Мотора», который проводится в Бресте.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Feature Section end -->
+
+<!-- Footer Section -->
+<footer class="footer-section">
+    <div class="container">
+        <a href="index.html" class="footer-logo">
+            <h3 class="text-white">BIKE-RIDE</h3></a>
+        <p>Движение осуществляется колонной, разбитой на команды по 30-40 человек. У каждой команды есть куратор –
+            волонтер, который выстраивает команду по 5-6 велосипедистов в ряд. Участники команды дружно едут вместе, не
+            обгоняя друг друга и другие команды.</p>
+        <p>
+            Участник не должен выезжать на встречную полосу. За выезд на встречную полосу - ШТРАФ 500 рублей и ЗАПРЕТ НА
+            УЧАСТИЕ В ВЕЛОПРОБЕГЕ на всех следующих велоднях!</p>
+        <div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+            Все права защищены | BIKE-RiDE <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="/"
+                                                                                                  target="_blank">RidersCommunity</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+    </div>
+</footer>
+<!-- Footer Section end -->
+
+<!--====== Javascripts & Jquery ======-->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.slicknav.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
